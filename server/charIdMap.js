1bb8790b46326c0a95e1315902442df259fa00eb
Ibnat Hossain <IBNAT.HOSSAIN@gmail.com>
2022-02-18 01:43:37 -0500

    Add files via upload

---
+++ server/charIdMap.js
@@ -0,0 +1,236 @@
+module.exports = {
+	"andy": {
+		"id": "andy",
+		"name": "Andy",
+		"full": "Andy Bogard",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Andy_Bogard"
+	},
+	"angel": {
+		"id": "angel",
+		"name": "Àngel",
+		"full": "Àngel",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/%C3%81ngel"
+	},
+	"antonov": {
+		"id": "antonov",
+		"name": "Antonov",
+		"full": "Antonov",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Antonov"
+	},
+	"ash": {
+		"id": "ash",
+		"name": "Ash",
+		"full": "Ash Crimson",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Ash_Crimson"
+	},
+	"athena": {
+		"id": "athena",
+		"name": "Athena",
+		"full": "Athena Asamiya",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Athena_Asamiya"
+	},
+	"benimaru": {
+		"id": "benimaru",
+		"name": "Benimaru",
+		"full": "Benimaru Nikaido",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Benimaru_Nikaido"
+	},
+	"blue_mary": {
+		"id": "blue_mary",
+		"name": "Blue Mary",
+		"full": "Blue Mary",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Blue_Mary"
+	},
+	"chizuru": {
+		"id": "chizuru",
+		"name": "Chizuru",
+		"full": "Chizuru Kagura",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Chizuru_Kagura"
+	},
+	"chris": {
+		"id": "chris",
+		"name": "Chris",
+		"full": "Chris",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Chris"
+	},
+	"clark": {
+		"id": "clark",
+		"name": "Clark",
+		"full": "Clark Still",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Clark_Still"
+	},
+	"dolores": {
+		"id": "dolores",
+		"name": "Dolores",
+		"full": "Dolores",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Dolores"
+	},
+	"elisabeth": {
+		"id": "elisabeth",
+		"name": "Elisabeth",
+		"full": "Elisabeth Blantorche",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Elisabeth_Blanctorche"
+	},
+	"heidern": {
+		"id": "heidern",
+		"name": "Heidern",
+		"full": "Heidern",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Heidern"
+	},
+	"iori": {
+		"id": "iori",
+		"name": "Iori",
+		"full": "Iori Yagami",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Iori_Yagami"
+	},
+	"isla": {
+		"id": "isla",
+		"name": "Isla",
+		"full": "Isla",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Isla"
+	},
+	"joe": {
+		"id": "joe",
+		"name": "Joe",
+		"full": "Joe Higashi",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Joe_Higashi"
+	},
+	"k": {
+		"id": "k",
+		"name": "K'",
+		"full": "K'",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/K%27"
+	},
+	"king": {
+		"id": "king",
+		"name": "King",
+		"full": "King",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/King"
+	},
+	"king_of_dinosaurs": {
+		"id": "king_of_dinosaurs",
+		"name": "King of Dinosaurs",
+		"full": "King of Dinosaurs",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/King_of_Dinosaurs"
+	},
+	"krohnen": {
+		"id": "krohnen",
+		"name": "Krohnen",
+		"full": "Krohnen",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Krohnen"
+	},
+	"kukri": {
+		"id": "kukri",
+		"name": "Kukri",
+		"full": "Kukri",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Kukri"
+	},
+	"kula": {
+		"id": "kula",
+		"name": "Kula",
+		"full": "Kula Diamond",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Kula_Diamond"
+	},
+	"kyo": {
+		"id": "kyo",
+		"name": "Kyo",
+		"full": "Kyo Kusanagi",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Kyo_Kusanagi"
+	},
+	"leona": {
+		"id": "leona",
+		"name": "Leona",
+		"full": "Leona Heidern",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Leona_Heidern"
+	},
+	"luong": {
+		"id": "luong",
+		"name": "Luong",
+		"full": "Luong",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Luong"
+	},
+	"mai": {
+		"id": "mai",
+		"name": "Mai",
+		"full": "Mai Shiranui",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Mai_Shiranui"
+	},
+	"maxima": {
+		"id": "maxima",
+		"name": "Maxima",
+		"full": "Maxima",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Maxima"
+	},
+	"meitenkun": {
+		"id": "meitenkun",
+		"name": "Meitenkun",
+		"full": "Meitenkun",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Meitenkun"
+	},
+	"ralf": {
+		"id": "ralf",
+		"name": "Ralf",
+		"full": "Ralf Jones",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Ralf_Jones"
+	},
+	"ramon": {
+		"id": "ramon",
+		"name": "Ramón",
+		"full": "Ramón",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Ram%C3%B3n"
+	},
+	"robert": {
+		"id": "robert",
+		"name": "Robert",
+		"full": "Robert Garcia",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Robert_Garcia"
+	},
+	"ryo": {
+		"id": "ryo",
+		"name": "Ryo",
+		"full": "Ryo Sakazaki",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Ryo_Sakazaki"
+	},
+	"shermie": {
+		"id": "shermie",
+		"name": "Shermie",
+		"full": "Shermie",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Shermie"
+	},
+	"shunei": {
+		"id": "shunei",
+		"name": "Shun'ei",
+		"full": "Shun'ei",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Shun%27ei"
+	},
+	"terry": {
+		"id": "terry",
+		"name": "Terry",
+		"full": "Terry Bogard",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Terry_Bogard"
+	},
+	"vanessa": {
+		"id": "vanessa",
+		"name": "Vanessa",
+		"full": "Vanessa",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Vanessa"
+	},
+	"whip": {
+		"id": "whip",
+		"name": "Whip",
+		"full": "Whip",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Whip"
+	},
+	"yashiro": {
+		"id": "yashiro",
+		"name": "Yashiro",
+		"full": "Yashiro Nanakasa",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Yashiro_Nanakase"
+	},
+	"yuri": {
+		"id": "yuri",
+		"name": "Yuri",
+		"full": "Yuri Sakazaki",
+		"wiki": "https://dreamcancel.com/wiki/index.php/The_King_of_Fighters_XV/Yuri_Sakazaki"
+	}
+};
